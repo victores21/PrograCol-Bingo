75026784c28314090179576befe31c3a6085c85a
Victor Escalona <victorescalonag@gmail.com>
2020-04-07 23:11:45 -0500

    Login and Profile responsive and with side menu working

--- bingo/Static-web/SideNavbar/scripts.js
+++ bingo/Static-web/SideNavbar/scripts.js
@@ -1,9 +1,25 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "350px";
-  document.getElementById("main").style.marginLeft = "350px";
+  document.getElementById("left-navbar").style.width = "300px";
+  document.getElementById("main").style.marginLeft = "300px";
 }
 
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("left-navbar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+}
+
+//Media query
+if (window.matchMedia("(max-width: 900px)").matches) {
+  /* The viewport is less than, or equal to, 700 pixels wide */
+  function openNav() {
+    document.getElementById("left-navbar").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+  function closeNav() {
+    document.getElementById("left-navbar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+} else {
+  /* The viewport is greater than 700 pixels wide */
 }
