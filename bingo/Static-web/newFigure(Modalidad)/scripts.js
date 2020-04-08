80e1353257a14990df37cbb39fc3989142fa6164
Victor Escalona <victorescalonag@gmail.com>
2020-04-08 18:55:18 -0500

    AddFigure static view done (responsive)

---
+++ bingo/Static-web/newFigure(Modalidad)/scripts.js
@@ -0,0 +1,31 @@
+function openNav() {
+  document.getElementById("left-navbar").style.width = "300px";
+  document.getElementById("main").style.marginLeft = "300px";
+}
+
+function closeNav() {
+  document.getElementById("left-navbar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+//Media query
+if (window.matchMedia("(max-width: 900px)").matches) {
+  /* The viewport is less than, or equal to, 700 pixels wide */
+  function openNav() {
+    document.getElementById("left-navbar").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+  function closeNav() {
+    document.getElementById("left-navbar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+} else {
+  /* The viewport is greater than 700 pixels wide */
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function mobileMenuOpen() {
+  document.getElementById("gmDropdown").classList.toggle("show");
+}
